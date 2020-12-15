49c27d8908c91294bebe56a7695f5708d0904bbf
Ravid Levi <61316827+RavidLevi98@users.noreply.github.com>
2020-12-15 16:09:57 +0200

    Add files via upload

---
+++ myMath.h
@@ -0,0 +1,9 @@
+#ifndef _MYMATH_H
+#define _MYMATH_H
+float add (float x , float y); 
+float sub(float x,float y);
+float mul(float x,float y );
+float div(float x,float y);
+double Power(double x, int y);
+double Exponent(int x);
+#endif
